2996690e2b992571f51b05c0d877f01986b6ab67
Colin Dean <colindean@users.noreply.github.com>
2022-07-03 21:02:17 -0400

    Convert python tax rates from silly class methods to ABC/statics

--- python_plot/tax_rates.py
+++ python_plot/tax_rates.py
@@ -1,94 +1,105 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
 from typing import Callable
 
 
-class TaxRates:
-    def mils(amt: float) -> Callable[[float], float]:
-        return lambda x: x * (amt / 1000.0)
+def mils(amt: float) -> Callable[[float], float]:
+    return lambda x: x * (amt / 1000.0)
 
-    def percent(amt: float) -> Callable[[float], float]:
-        return lambda x: x * (amt / 100.0)
+def percent(amt: float) -> Callable[[float], float]:
+    return lambda x: x * (amt / 100.0)
 
+
+class TaxRates(ABC):
+
+    @abstractmethod
+    @staticmethod
     def wage_tax(wages: float) -> float:
         pass
 
+    @abstractmethod
+    @staticmethod
     def property_tax(assessment: float) -> float:
         pass
 
 
 class AlleghenyCounty(TaxRates):
+    @staticmethod
     def wage_tax(_) -> float:
         """
         Allegheny County does not assess a wage tax.
         """
         return 0.0
 
-    @classmethod
-    def property_tax(cls, assessment: float) -> float:
-        return cls.mils(4.73)(assessment)
+    @staticmethod
+    def property_tax(assessment: float) -> float:
+        return mils(4.73)(assessment)
 
 
 class WilkinsburgSchools(TaxRates):
-    @classmethod
-    def wage_tax(cls, wages: float) -> float:
-        return cls.percent(0.5)(wages)
+    @staticmethod
+    def wage_tax(wages: float) -> float:
+        return percent(0.5)(wages)
 
-    @classmethod
-    def property_tax(cls, assessment: float) -> float:
-        return cls.mils(24.5)(assessment)
+    @staticmethod
+    def property_tax(assessment: float) -> float:
+        return mils(24.5)(assessment)
 
 
 class WilkinsburgBorough(TaxRates):
 
-    @classmethod
-    def wage_tax(cls, wages: float) -> float:
-        return cls.percent(0.5)(wages)
+    @staticmethod
+    def wage_tax(wages: float) -> float:
+        return percent(0.5)(wages)
 
-    @classmethod
-    def property_tax(cls, assessment: float) -> float:
-        return cls.mils(14.0)(assessment)
+    @staticmethod
+    def property_tax(assessment: float) -> float:
+        return mils(14.0)(assessment)
 
 
 class Wilkinsburg(TaxRates):
-    @classmethod
-    def wage_tax(cls, wages: float) -> float:
+    @staticmethod
+    def wage_tax(wages: float) -> float:
         return WilkinsburgBorough.wage_tax(wages) + WilkinsburgSchools.wage_tax(wages)
 
-    @classmethod
-    def property_tax(cls, assessment: float) -> float:
+    @staticmethod
+    def property_tax(assessment: float) -> float:
         return WilkinsburgBorough.property_tax(assessment) + WilkinsburgSchools.property_tax(assessment)
 
 
 class PittsburghSchools(TaxRates):
-    @classmethod
-    def wage_tax(cls, wages: float) -> float:
-        return cls.percent(2)(wages)
+    @staticmethod
+    def wage_tax(wages: float) -> float:
+        return percent(2)(wages)
 
-    @classmethod
-    def property_tax(cls, assessment: float) -> float:
-        return cls.mils(10.25)(assessment)
+    @staticmethod
+    def property_tax(assessment: float) -> float:
+        return mils(10.25)(assessment)
 
 
 class PittsburghCity(TaxRates):
-    @classmethod
-    def wage_tax(cls, wages: float) -> float:
-        return cls.percent(1)(wages)
+    @staticmethod
+    def wage_tax(wages: float) -> float:
+        return percent(1)(wages)
 
-    @classmethod
-    def property_tax(cls, assessment: float) -> float:
-        parks = cls.mils(0.5)
-        library = cls.mils(0.25)
-        re_tax = cls.mils(8.06)
+   @staticmethod
+    def property_tax(assessment: float) -> float:
+        parks = mils(0.5)
+        library = mils(0.25)
+        re_tax = mils(8.06)
         taxes = [parks, library, re_tax]
         tax_amounts = map(lambda tax: tax(assessment), taxes)
         return sum(tax_amounts)
 
 
 class Pittsburgh(TaxRates):
-    @classmethod
-    def wage_tax(cls, wages: float) -> float:
+    @staticmethod
+    def wage_tax(wages: float) -> float:
         return PittsburghSchools.wage_tax(wages) + PittsburghCity.wage_tax(wages)
 
-    @classmethod
-    def property_tax(cls, assessment: float) -> float:
+    @staticmethod
+    def property_tax(assessment: float) -> float:
         return PittsburghSchools.property_tax(assessment) + PittsburghCity.property_tax(assessment)
 
